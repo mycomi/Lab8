dc3ba7fe04b966e77bb2365a4610b7032887f6ed
mycomi <mycomi20th@gmail.com>
2020-03-24 17:55:37 +0700

    Updates

---
+++ precache-manifest.877c97f5052414b56d583ece38ec57bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90081eb3f3b381988b2820ce7af56326",
+    "url": "/Lab8/index.html"
+  },
+  {
+    "revision": "47aed2ecfb9fe7ff9231",
+    "url": "/Lab8/static/css/main.4851e9fc.chunk.css"
+  },
+  {
+    "revision": "4a4ccd0041ee12827329",
+    "url": "/Lab8/static/js/2.58e01cf0.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/Lab8/static/js/2.58e01cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47aed2ecfb9fe7ff9231",
+    "url": "/Lab8/static/js/main.665aabb3.chunk.js"
+  },
+  {
+    "revision": "617a658e02b06f0fe67a",
+    "url": "/Lab8/static/js/runtime-main.7a019697.js"
+  }
+]);
